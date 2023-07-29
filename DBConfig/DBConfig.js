4885416bf1b162869fe108691875deeb65b1ec32
MINHAJ SINAN V P <minhajsinanvp@gmail.com>
2023-07-29 18:35:53 +0530

    mongdb sever is connected

---
+++ DBConfig/DBConfig.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+
+
+
+
+mongoose.connect(process.env.DB_URL)
+
+const connection = mongoose.connection;
+
+
+connection.on("connected", ()=>{
+    console.log("MongoDB is connected");
+})
+
+
+
+connection.on("erro", (error)=>{
+    console.log("error is occuredddddddddddddddddd");
+})
+
+
+module.exports = mongoose;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+require("dotenv").config()
 
+const dbConfig = require('./DBConfig/DBConfig')
 const port = process.env.PORT || 5000  // solution for the port being used
 
 
